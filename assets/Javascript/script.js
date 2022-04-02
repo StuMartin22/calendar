d1dad8e607cd4cc589df9b0abc2ebf3452cc006e
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-02 15:09:47 -0500

    text now stores in local storage and will remain on page reload. Began work on getting text input areas to change color based on time relation to current hour.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,9 +1,9 @@
 var currentDate = moment().format("dddd, MMMM Do");
 var currentDateDisplay = $("#currentDay");
 currentDateDisplay.text(currentDate);
-var saveBtn = $('#saveBtn')
-var textArea = $('.text')
+var saveBtn = $('.saveBtn')
 var hour = $('.hour')
+var rows = $('.row')
 
 
 // saveBtn.on('click', function () {
@@ -18,3 +18,37 @@ var hour = $('.hour')
 // console.log(textArea.val());
 
 // for (var i=0; i<9 hour.length; i++;
+
+
+
+for (let i = 0; i < rows.length; i++) {
+    var button = saveBtn[i]
+    button.addEventListener('click', function(){
+    localStorage.setItem("data" + i,rows[i].children[1].value)
+    });
+} 
+
+for (let i = 0; i < rows.length; i++) {
+    rows[i].children[1].textContent=localStorage.getItem('data' + i)
+};
+
+//TODO: Below is work in progress of color-time correlation
+// //if currentday = row.hour red
+// //else if currentday>hour green
+// //else if currentday<hour gray 
+
+// var timeNow = moment().format("H")
+
+// // if (rows[i].children[0].textContent == currentDateDisplay
+// //     rows[i].children[1]
+
+//     var date = new Date();
+// console.log(date.toLocaleString('en-US'));
+// date.gethours()
+// console.log(Date);
+
+// var options = { hour12: false };
+// console.log(date.toLocaleString('en-US', options));
+
+// // var testers = gethours()
+// // console.log(testers);
